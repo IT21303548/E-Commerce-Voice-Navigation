483560f1261066532c9d041f95d557d30ab076cb
Tharaka Arawinda <99238128+IT21303548@users.noreply.github.com>
2024-10-09 11:49:34 +0530

    Done backend

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+
+const categoryRoutes = require("./routes/categoryRoutes");
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
+const genreRoutes = require("./routes/genreRoutes");
+const ratingRoutes = require("./routes/ratingRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const reportRoutes = require("./routes/reportRoutes");
+const imageRoutes = require("./routes/imageRoutes");
+const miniImageRoutes = require("./routes/miniImageRoutes");
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// MIDDLEWARES
+app.use(cors());
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json());
+
+// ROUTES
+app.use("/users", userRoutes);
+app.use("/categories", categoryRoutes);
+app.use("/genres", genreRoutes);
+app.use("/products", productRoutes);
+app.use("/ratings", ratingRoutes);
+app.use("/comments", commentRoutes);
+app.use("/orders", orderRoutes);
+app.use("/reports", reportRoutes);
+app.use("/images", imageRoutes);
+app.use("/minis", miniImageRoutes);
+
+mongoose.connect(process.env.MONGODB_URL, () => {
+  console.log("Successfully connected to database.");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
